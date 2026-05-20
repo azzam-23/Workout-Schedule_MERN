56af602f458b443b265e816f0867008e58363ecb
Azzam <azam151523@gmail.com>
2026-05-20 16:13:10 +0300

    fix render port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute.js";
 import workoutScheduleRoute from "./routes/workoutScheduleRoute.ts";
-import  cors from "cors";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,21 +11,16 @@ console.log(process.env.MONGO_URI);
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(cors());
 mongoose
   .connect(process.env.MONGO_URI || "")
   .then(() => console.log("mongo connected!"))
   .catch((err) => console.log("Failed to connect!", err));
-  
 
+app.use("/user", userRoute);
+app.use("/workout-schedule", workoutScheduleRoute);
 
-
-
-
-  app.use("/user", userRoute);
-  app.use("/workout-schedule", workoutScheduleRoute);
-  app.listen(port, () => {
-    console.log(`"Server is running at: http://localhost:${port}`)
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
